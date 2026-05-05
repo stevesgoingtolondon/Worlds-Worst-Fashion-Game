e9090a38484473372f09ec23ed17cde0e43011b3
Kat Morris <kemorris2@my.waketech.edu>
2026-05-05 13:39:06 -0400

    Fix clothing data and update game prompts

--- script.js
+++ script.js
@@ -26,31 +26,100 @@ const gamePrompts = [ "Jury Duty",
     "Preforming with his brothers band.",
     "Experincing Ross Dress For Less for the first time."
 ];
+var gamePromts = gamePrompts[Math.floor(Math.random()*items.length)];
 
 
 const clothingData = {
+ //Setting up the shirts 'n pants 'n stuff guhhhhh
   shirt: [
     { name: "Stars' Shirt", img: "starsshirt.png" },
-    { name: "Button down Shirt", img: "shirt2.png" }
+    { name: "Button down Shirt", img: "buttondown.png" }
+    { name: "White Boy Of The Year", img: "wboty.png" }
   ],
 
     pants: [
-    { name: "pants1", img: "pants1" },
-    { name: "pants2", img: "pants2.png" }
+    { name: "Pajama Pants", img: "pjpants.png" },
+    { name: "Short Shorts", img: "shortshorts.png" }
+    { name: "Grey Sweat Pants", img: "greysweatpants.png" }
   ],
 
       shoes: [
-    { name: "shoes1", img: "shoes1" },
-    { name: "shoes2", img: "shoes2.png" }
+    { name: "Bunny Slippers", img: "bunnyslippers.png" },
+    { name: "Brown Shoes", img: "brownshoes.png" }
+    { name: "Slides", img: "slides.png" }
   ],
 
-        acessiores: [
-    { name: "shoes1", img: "shoes1" },
-    { name: "shoes2", img: "shoes2.png" }
-  ],
+       // acessiores: [
+    //{ name: "shoes1", img: "shoes1" },
+    //{ name: "shoes2", img: "shoes2.png" }
+  //],
 }
 
-var gamePromts = gamePrompts[Math.floor(Math.random()*items.length)];
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/null
+let outfit = {
+  shirt: null,
+  pants: null,
+  shoes: null,
+  accessory: null
+};
+
+let timer;
+let timeLeft = 30;
+
+//Function to start the game and timer
+function startGame() {
+  clearInterval(timer);
+  timeLeft = 30;
+  document.getElementById("timer").textContent = timeLeft;
+  //Hello prompt
+  generatePrompt();
+  //Hello Store
+  generateStore();
+  //Ken appears in his underwear again. poor guy.
+  resetOutfit();
+
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+  timer = setInterval(() => {
+    timeLeft;
+    document.getElementById("timer")
+
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      submitOutfit();
+    }
+  }, 1000);
+}
+
+function generatePrompt() {
+  const random = prompts[Math.floor(Math.random() * prompts.length)];
+//https://stackoverflow.com/questions/5915096/get-a-random-item-from-a-javascript-array
+  document.getElementById("prompt").textContent = random;
+}
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+function generateStore() {
+  const store = document.getElementById("store");
+  store.innerHTML = "";
+}
+
+function generateStore() {
+  const store = document.getElementById("store");
+  store.innerHTML = "";
+
+  for (let category in clothingData) {
+    for (let item of clothingData[category]) {
+      const img = document.createElement("img");
+      img.src = item.img;
+      img.className = "item";
+
+      img.onclick = () => selectItem(category, item, img);
+
+      store.appendChild(img);
+    }
+  }
+
+
+
 
 //Each clothing item will be looped over while being loaded with DOM.
 
